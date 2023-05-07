0f7404ce22ad1db3c2478a7145d94e92b9ea2347
am999 <am9999072080@gmail.com>
2023-05-07 16:13:16 +0300

    commit1

--- src/main/java/ru/skyproHom2_5Collection_List/service/EmployeeServiceImpl.java
+++ src/main/java/ru/skyproHom2_5Collection_List/service/EmployeeServiceImpl.java
@@ -12,5 +12,46 @@ import java.util.Set;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+    private  static  final  int Employees_MAX_Count=5;
+    private final Set<Employee>employees;
+    public EmployeeServiceImpl() {
+        this.employees = new HashSet<>();
+    }
 
+    @Override
+    public Employee add(String firstName, String lastName) {
+        Employee employee=new Employee(firstName, lastName);
+        if (employees.contains(employee)){
+            throw new EmployeeAlreadyAddedException(employee);
+    }
+        if(employees.size()==Employees_MAX_Count){
+            throw new EmployeeStorageIsFullException();
+        }
+        employees.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee remove(String firstName, String lastName) {
+        Employee employee=new Employee(firstName, lastName);
+        if (employees.contains(employee)){
+            throw new EmployeeNotFoundException(employee);
+        }
+        employees.remove(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee find(String firstName, String lastName) {
+        Employee employee=new Employee(firstName, lastName);
+        if (employees.contains(employee)) {
+            throw new EmployeeNotFoundException(employee);
+        }
+        return employee;
+    }
+
+    @Override
+    public Collection<Employee> findAll() {
+        return employees;
+    }
 }
